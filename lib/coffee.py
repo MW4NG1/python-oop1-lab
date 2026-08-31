af3afcbf29493c8b700aebae2d8c7e7fc670da7b
MW4NG1 <msage3803@gmail.com>
2026-08-31 16:35:46 +0300

    Implement Coffee class with initialization and tipping functionality

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        # Check that the size is valid
+        if size in ["Small", "Medium", "Large"]:
+            self.size = size
+        else:
+            print("size must be Small, Medium, or Large")
+
+        # Store the coffee price
+        self.price = price
+
+    def tip(self):
+        # Print the required message and add 1 to the price
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
