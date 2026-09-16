ed33fade25a009313c4261eaa7899a79aff0a773
MW4NG1 <msage3803@gmail.com>
2026-09-16 13:38:50 +0300

    Refactor Book and Coffee classes to set default parameters for title, page_count, size, and price

--- lib/book.py
+++ lib/book.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self, title, page_count):
+    def __init__(self, title="Untitled", page_count=100):
         self.title = title
-
-        if isinstance(page_count, int):
-            self.page_count = page_count
-        else:
-            print("page_count must be an integer")
+        self.page_count = page_count
 
     def turn_page(self):
-        print("Flipping the page...wow, you read fast!")
+        # Increment page count when turning a page
+        self.page_count += 1
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    def __init__(self, size, price):
+    def __init__(self, size="Small", price=0.0):
         # Check that the size is valid
         if size in ["Small", "Medium", "Large"]:
             self.size = size
         else:
             print("size must be Small, Medium, or Large")
+            self.size = size
 
         # Store the coffee price
         self.price = price
