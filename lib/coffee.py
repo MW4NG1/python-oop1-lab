a7609cff3cbeb43ba9d52319a02f42e11820bc8a
MW4NG1 <msage3803@gmail.com>
2026-09-16 13:40:55 +0300

    Refactor Book and Coffee classes to implement property setters for title, page_count, size, and price; update initialization parameters to remove defaults

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    def __init__(self, size="Small", price=0.0):
-        # Check that the size is valid
-        if size in ["Small", "Medium", "Large"]:
-            self.size = size
-        else:
-            print("size must be Small, Medium, or Large")
-            self.size = size
-
-        # Store the coffee price
+    def __init__(self, size, price):
+        self.size = size
         self.price = price
 
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, val):
+        if val in ["Small", "Medium", "Large"]:
+            self._size = val
+        else:
+            print("size must be Small, Medium, or Large")
+
     def tip(self):
-        # Print the required message and add 1 to the price
         print("This coffee is great, here’s a tip!")
         self.price += 1
